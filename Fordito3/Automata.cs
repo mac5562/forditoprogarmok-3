4bd2f67b11f311cbc31be70181e766e662371b38
mac5562 <55428222+mac5562@users.noreply.github.com>
2020-11-29 23:22:52 +0100

    projekt feltöltése Signed-off-by: mac5562 <55428222+mac5562@users.noreply.github.com>

---
+++ Fordito3/Automata.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Fordito3
+{
+    class Automata
+    {
+        string input;
+        int i;
+
+        public string simple(string st)
+        {
+            return Regex.Replace(st,"([0-9]{1,2})+","i");
+        }
+        public void elfogad(char ch)
+        {
+            if (input[i] != ch)
+            {
+                Console.WriteLine("Hibás kifejetés {0}. Helyes karakter: {1}",input,input[i]);
+            }
+            i++;
+        }
+
+        public Automata(string input)
+        {
+            Console.WriteLine("Eredeti input: {0}", input);
+            this.input = $"{simple(input)}#";
+            Console.WriteLine("Az egyszerűsített input: {0}", this.input);
+        }
+
+        public void S()
+        {
+            E();
+            elfogad('#');
+            Console.WriteLine("Az elemzés lefutott");
+        }
+        void E()
+        {
+            T();
+            Ev();
+        }
+        void T()
+        {
+            F();
+            Tv();
+        }
+        void Ev()
+        {
+            if (input[i] == '+')
+            {
+                elfogad('+');
+                T();
+                Ev();
+            }
+        }
+        void Tv()
+        {
+            if (input[i]=='*')
+            {
+                elfogad('*');
+                F();
+                Tv();
+            }
+        }
+
+        void F()
+        {
+            if (input[i] == '(')
+            {
+                elfogad('(');
+                E();
+                elfogad(')');
+            }
+            else
+            {
+                elfogad('i');
+            }
+        }
+    }
+}
---
+++ Fordito3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fordito3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Automata a = new Automata("(31+233)*a+4");
+            a.S();
+            Console.ReadLine();
+        }
+    }
+}
